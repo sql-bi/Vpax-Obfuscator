fb4310586999eb38eef9f111869ca5420752b148
Alberto Spelta <albertospelta@users.noreply.github.com>
2024-03-06 10:29:41 +0100

    Move IsSquareBraketsRequired into ObfuscateFullyQualifiedColumnName

--- src/Dax.Vpax.Obfuscator/DaxModelObfuscator.ObfuscateExpression.cs
+++ src/Dax.Vpax.Obfuscator/DaxModelObfuscator.ObfuscateExpression.cs
@@ -68,10 +68,40 @@ internal sealed partial class DaxModelObfuscator
 
     internal string ObfuscateFullyQualifiedColumnName(string value)
     {
-        var (table, column) = value.GetFullyQualifiedColumnNameParts(obfuscating: true);
-        var tableName = ObfuscateText(new DaxText(table)).ObfuscatedValue;
-        var columnName = ObfuscateText(new DaxText(column)).ObfuscatedValue;
+        var (table, column) = value.GetFullyQualifiedColumnNameParts();
+
+        var tableText = new DaxText(table);
+        var columnText = new DaxText(column);
+
+        if (IsSquareBraketsRequired(table, column))
+        {
+            // Since the plaintext value contains at least one character that results in a fully qualified
+            // column name that requires square brackets, then, in order to preserve the same semantics
+            // we must add at least a single char of the same type to the obfuscated value as well.
+            // We use the reserved char '-' for this purpose becase it is preserved by the obfuscator.
+            tableText = new DaxText($"{DaxTextObfuscator.ReservedChar_Minus}{table}");
+            tableText.PlaintextValue = table;
+        }
+
+        var tableName = ObfuscateText(tableText).ObfuscatedValue;
+        var columnName = ObfuscateText(columnText).ObfuscatedValue;
 
         return $"{tableName}[{columnName}]";
+
+        static bool IsSquareBraketsRequired(string table, string column)
+        {
+            if (table.Length > 0)
+            {
+                table = table.Trim(); // remove any leading or trailing whitespace
+
+                if ("0123456789".Contains(table[0]))
+                    return true; // Table name start with a digit
+
+                if (table.Any((c) => c != '_' && !DaxTextObfuscator.CharSet.Contains(c)))
+                    return true; // Table name contains any non-alphabetic characters except for the underscore
+            }
+
+            return column.Contains(']');
+        }
     }
 }
--- src/Dax.Vpax.Obfuscator/Extensions/StringExtensions.cs
+++ src/Dax.Vpax.Obfuscator/Extensions/StringExtensions.cs
@@ -11,7 +11,7 @@ internal static class StringExtensions
     public static bool IsFullyQualifiedColumnName(this string value)
         => value.TrimEnd().EndsWith("]") && value.IndexOf('[') > 0;
 
-    public static (string table, string column) GetFullyQualifiedColumnNameParts(this string value, bool obfuscating = false)
+    public static (string table, string column) GetFullyQualifiedColumnNameParts(this string value)
     {
         Debug.Assert(IsFullyQualifiedColumnName(value));
 
@@ -20,34 +20,7 @@ internal static class StringExtensions
         var table = value.Substring(0, openIndex);
         var column = value.Substring(openIndex + 1, closeIndex - openIndex - 1);
 
-        if (obfuscating)
-        {
-            table = table.Trim(); // remove any leading or trailing whitespace first
-
-            if (IsSquareBraketsRequired(table, column))
-            {
-                // Since the plaintext value contains at least one character that results in a fully qualified
-                // column name that requires square brackets, then, in order to preserve the same semantics
-                // we must add at least a single char of the same type to the obfuscated value as well.
-                table = $"{DaxTextObfuscator.ReservedChar_Minus}{table}";
-            }
-        }
-
         return (table, column);
-
-        static bool IsSquareBraketsRequired(string table, string column)
-        {
-            if (table.Length > 0)
-            {
-                if ("0123456789".Contains(table[0]))
-                    return true; // Table name start with a digit
-
-                if (table.Any((c) => c != '_' && !DaxTextObfuscator.CharSet.Contains(c)))
-                    return true; // Table name contains any non-alphabetic characters except for the underscore
-            }
-
-            return column.Contains(']');
-        }
     }
 
     public static string EscapeDax(this string value, int tokenType)
