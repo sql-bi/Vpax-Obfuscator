ecdc9f1fe26e09c8da45e3dcfb6682c31adf1a75
Alberto Spelta <albertospelta@users.noreply.github.com>
2024-03-06 10:35:21 +0100

    Refactor ObfuscateText(DaxText)

--- src/Dax.Vpax.Obfuscator/DaxModelObfuscator.ObfuscateText.cs
+++ src/Dax.Vpax.Obfuscator/DaxModelObfuscator.ObfuscateText.cs
@@ -9,19 +9,19 @@ internal sealed partial class DaxModelObfuscator
         if (Texts.TryGet(text, out var obfuscatedText))
             return obfuscatedText ?? throw new InvalidOperationException($"Obfuscated text is null. {text.Value}");
 
-        obfuscatedText = _obfuscator.Obfuscate(text);
+        _obfuscator.Obfuscate(text);
 
         var retryLimit = DaxTextObfuscator.RetryLimitBeforeExtension + 100; // retry with the same length otherwise extend the obfuscated string length up to 100 characters
         var retryCount = 0;
         while (retryCount < retryLimit && IsRetryNeeded())
-            obfuscatedText = _obfuscator.Obfuscate(obfuscatedText, ++retryCount);
+            _obfuscator.Obfuscate(text, ++retryCount);
 
         if (retryCount >= retryLimit)
-            throw new InvalidOperationException($"Failed to obfuscate text. {obfuscatedText.Value} | {obfuscatedText.ObfuscatedValue}");
+            throw new InvalidOperationException($"Failed to obfuscate text. {text.Value} | {text.ObfuscatedValue}");
 
-        Texts.Add(obfuscatedText); // << throws in case of unresolved collision (duplicate value/obfuscated value)
-        return obfuscatedText;
+        Texts.Add(text); // << throws in case of unresolved collision (duplicate value/obfuscated value)
+        return text;
 
-        bool IsRetryNeeded() => obfuscatedText.ObfuscatedValue.IsDaxKeyword() || Texts.Contains(obfuscatedText);
+        bool IsRetryNeeded() => text.ObfuscatedValue.IsDaxKeyword() || Texts.Contains(text);
     }
 }
