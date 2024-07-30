132439af4ee42dec7ff36b3dd39fe78a6baf1f1a
Alberto Spelta <albertospelta@users.noreply.github.com>
2024-07-30 10:32:46 +0000

    Update tokenizer with visual shape keywords (#56)
    * Refactor, format code
    
    * Add null-coalescing assignment to `dax` expression string in `TokenizeImpl`
    
    * Update DaxLexer/DaxToken.generated.cs
    
    * Add `TOTAL` to DaxKeywords

--- src/Dax.Tokenizer/DaxTokenizer.cs
+++ src/Dax.Tokenizer/DaxTokenizer.cs
@@ -14,16 +14,19 @@ namespace Dax.Tokenizer
 
         private static List<IToken> TokenizeImpl(string dax, DaxLocale locale, bool includeHidden)
         {
+            dax ??= string.Empty;
             var input = new DaxCharStream(dax);
 
             var lexer = new DaxLexer(input, locale);
             var tokenStream = new BufferedTokenStream(lexer);
             lexer.RemoveErrorListeners();
             var tokens = new List<IToken>(includeHidden ? Math.Min(dax.Length / 3, 32) : Math.Min(dax.Length / 4, 16));
-            if (includeHidden) {
+            if (includeHidden)
+            {
                 var t = lexer.NextToken();
                 int i = 0;
-                while (t.Type != TokenConstants.EOF) {
+                while (t.Type != TokenConstants.EOF)
+                {
                     var wt = (IWritableToken)t;
                     wt.TokenIndex = i;
                     wt.Line = t.Line - 1; // Shift ANTLRs line numbering (1..n) -> (0..n)
@@ -32,11 +35,14 @@ namespace Dax.Tokenizer
                     t = lexer.NextToken();
                 }
             }
-            else {
+            else
+            {
                 var t = lexer.NextToken();
                 int i = 0;
-                while (t.Type != TokenConstants.EOF) {
-                    if (t.Channel == DaxLexer.DefaultTokenChannel) {
+                while (t.Type != TokenConstants.EOF)
+                {
+                    if (t.Channel == DaxLexer.DefaultTokenChannel)
+                    {
                         var wt = (IWritableToken)t;
                         wt.TokenIndex = i;
                         wt.Line = t.Line - 1; // Shift ANTLRs line numbering (1..n) -> (0..n)
