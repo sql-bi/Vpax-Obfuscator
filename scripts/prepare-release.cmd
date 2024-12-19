1d7a7908d675b8e35e5f2ceb1da2cc549d0d813f
Alberto Spelta <albertospelta@users.noreply.github.com>
2024-12-19 11:00:43 +0100

    Add prepare-release.cmd script for automated release prep

---
+++ scripts/prepare-release.cmd
@@ -0,0 +1,9 @@
+@echo off
+
+rem Script to prepare a new release using the dotnet tool nbgv.
+rem Ensure nbgv is installed before running this script. Install it using `dotnet tool install -g nbgv`
+
+echo Running nbgv prepare-release...
+nbgv prepare-release --format json --commit-message-pattern "chore(release): bump version to {0}" || exit /b 1
+
+echo Release preparation is complete. IMPORTANT: Don't forget to push the newly created release branch (releases/*)
