eb444329e6ee6e5c3b81eee8c477ccdc9fec0224
Alberto Spelta <albertospelta@users.noreply.github.com>
2024-12-19 13:02:10 +0100

    Rename releases to release

--- scripts/prepare-release.cmd
+++ scripts/prepare-release.cmd
@@ -6,4 +6,4 @@ rem Ensure nbgv is installed before running this script. Install it using `dotne
 echo Running nbgv prepare-release...
 nbgv prepare-release --format json --commit-message-pattern "chore(release): bump version to {0}" || exit /b 1
 
-echo Release preparation is complete. IMPORTANT: Don't forget to push the newly created release branch (releases/*)
+echo Release preparation is complete. IMPORTANT: Don't forget to push the newly created release branch.
